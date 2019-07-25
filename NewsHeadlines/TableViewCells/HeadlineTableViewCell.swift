37de60000fbb1807486db395e669c92eafffe7fa
Raju VK <raju.kankanwadi@cox.com>
2019-07-26 00:19:01 +0530

    initial commit and add a tableViewCell

---
+++ NewsHeadlines/TableViewCells/HeadlineTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeadlineTableViewCell.swift
+//  NewsHeadlines
+//
+//  Created by raju.kankanwadi on 26/07/19.
+//  Copyright © 2019 raju.kankanwadi. All rights reserved.
+//
+
+import UIKit
+
+class HeadlineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
